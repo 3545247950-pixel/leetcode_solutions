4eea74be77e7fe56be77892f1aaa8cf5e44304ca
Asuka__s <3545247950@qq.com>
2026-04-21 20:49:16 +0800

    add stack(monotonic) 84_Largest_Rectangle.cpp

---
+++ stack/84_Largest_Rectangle.cpp
@@ -0,0 +1,36 @@
+﻿//单调栈解法
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> stk;
+        vector<int> left(n, -1);
+        vector<int> right(n, n);
+        for (int i = 0; i < n; i++)
+        {
+            while (!stk.empty() && heights[i] < heights[stk.top()])
+            {
+                right[stk.top()] = i;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        stk = stack<int>{};
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!stk.empty() && heights[i] < heights[stk.top()])
+            {
+                left[stk.top()] = i;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res = max(res, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return res;
+
+    }
+};
