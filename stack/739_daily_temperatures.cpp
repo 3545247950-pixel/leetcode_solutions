3600455c5648e90f2f35fd0f1ee29834b96dc6fe
Asuka__s <3545247950@qq.com>
2026-04-21 20:28:16 +0800

    stack(monotonic)

---
+++ stack/739_daily_temperatures.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> stk;     //单调栈
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            while (!stk.empty() && temperatures[i] > temperatures[stk.top()])
+            {
+                int idx = stk.top();
+                ans[idx] = i - idx;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        return ans;
+    }
+};
